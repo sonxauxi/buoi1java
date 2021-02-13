78f00ea74403ffbda8a61d24d7837829df02e22c
sonxauxi <76843729+sonxauxi@users.noreply.github.com>
2021-02-13 13:36:59 +0700

    up
    up

--- bmi/chanle.java
+++ bmi/chanle.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 public class chanle { 
     public static void main(String[]args ) throws Exception{
         Scanner input = new Scanner(System.in);
-        int a   ;
-        a = input.nextInt ();
+        System.out.println ( "Nhập vào a :");
+        int a = input.nextInt();
         /* if ( a % 2 == 0) {
             System.out.println( "a là số chẵn");            
         } else {
             System.out.println("a là số lẻ");
         } */
-      String b = (  a % 2 == 0) ? " a  là số chẵn"  : " a  là số lẻ" ;
+      String b = (  a % 2 == 0) ? "a  là số chẵn"  : "a  là số lẻ" ;
         System.out.println( b );
         input.close();
     }
